c5cb98553137695561919af4ae9d13a172791ef7
Rafael Switala <RafaelSwitala@onlineger.online-gmbh.de>
2025-04-15 16:13:45 +0200

    Optimized Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
-
 import './Footer.css';
 
 const Footer = () => {
   return (
     <footer className='footer'>
-      
+      <div className='footer-content'>
+        <div className='footer-left'>
+          <p><strong>CyberSecurity Dashboard</strong></p>
+          <p>Fallstudie Software Engineering</p>
+          <p>IU Internationale Hochschule</p>
+          <p>Prof. Alex Büchner</p>
+        </div>
+        <div className='footer-center'>
+          <p>Entwickelt von:</p>
+          <ul>
+            <li>Atakan Cevke</li>
+            <li>Aziz Mert Bektas</li>
+            <li>Rafael Switala</li>
+          </ul>
+        </div>
+        <div className='footer-right'>
+          <p>&copy; {new Date().getFullYear()} Cybersecurity Dashboard</p>
+          <a href='https://github.com/RafaelSwitala/CyberSecurityDashboard/' target='_blank' rel='noopener noreferrer'>
+            GitHub Repository
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
